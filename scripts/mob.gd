f30dc2e7f9ce48b4574106dc7ea72048d8684240
Dime <vinicius.costa.92@gmail.com>
2018-02-21 02:43:00 -0300

    alert states

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 export(NodePath) var patrol_start_path
+export(NodePath) var patrol_mid_path
 export(NodePath) var patrol_end_path
 export(NodePath) var player_path
 export(NodePath) var nav_path
@@ -10,74 +11,92 @@ onready var vision = get_node('vision/vision_range')
 onready var vision_light = get_node('vision')
 onready var vision_raycast = get_node('vision/RayCast2D')
 onready var effect = get_node("vision_effect")
+onready var path_timer = get_node('path_timer')
+onready var alert_timer = get_node('alert_timer')
 
 onready var patrol_start = get_node(patrol_start_path)
+onready var patrol_mid = get_node(patrol_mid_path)
 onready var patrol_end = get_node(patrol_end_path)
 onready var nav = get_node(nav_path) setget set_nav
 
-
-var speed = 50
+var alert_vel = 4
+var speed = 500
 var path = []
 var last_point
 var goal = Vector2()
-var alert = false
+var alert = false setget set_alert
 var is_visible = false
 
 var last_print = ''
 
 func _ready():
     self.modulate = Color(1, 1, 1, 0)
-    goal = patrol_end.position
+    goal = patrol_mid.position
     pass
-    
 
+func set_alert(new_alert):
+    alert_vel = 1
+    alert = new_alert
+    update_path()
+    
 func set_nav(new_nav):
     nav = new_nav
-    print(nav)
     update_path()
 #
-#func _draw():
-#    for p in path:
-#        if p == path[0]:
-#          last_point = p 
-#        elif path[path.size()-1] == p:
-#            pass
-#        else:
-#            draw_line(last_point-position, p-position, Color(255, 0, 0), 1)
-#            last_point = p
+func _draw():
+    for p in path:
+        if p == path[0]:
+          last_point = p 
+        elif path[path.size()-1] == p:
+            pass
+        else:
+            draw_line(last_point-position, p-position, Color(255, 0, 0), 1)
+            last_point = p
 
 func update_path():
-    goal = patrol_end.position
-    path = nav.get_simple_path(position, goal, false)
-    if path.size() == 0:
-        queue_free()
+#    goal = patrol_mid.position
+    if alert:
+        path = nav.get_simple_path(position, player.position, false)
+        path_timer.start()
+    else:
+        path = nav.get_simple_path(position, goal, false)
+#    if path.size() == 0:
+#        queue_free()
+
+func restart_timer():
+    path_timer.start()
         
 func fade_visibilty(in_out):
     var from = 0
-    var to = 1.5
+    var to = 1
     if in_out == 1:
         is_visible = true
     else:
         is_visible = false
-        from = 1.5
+        from = 1
         to = 0
         
     effect.interpolate_property(vision_light, 'energy', from, to, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
     effect.interpolate_property(self, "modulate", 
-            Color(1, 1, 1, from), Color(1, 1, 1, to-0.5), 0.2, 
+            Color(1, 1, 1, from), Color(1, 1, 1, to), 0.2, 
             Tween.TRANS_LINEAR, Tween.EASE_IN)
             
             
 func _process(delta):
-    
-    if position.distance_to(patrol_end.position) < 80:
+        
+    if position.distance_to(goal) < 100:
+        patrol_mid.position = patrol_end.position
         patrol_end.position = patrol_start.position
         patrol_start.position = goal
+        goal = patrol_mid.position
+        
+
         
     if path.size() > 1:
+        
         var d = position.distance_to(path[0])
-        if d > 2:
-            position = position.linear_interpolate(path[0], speed*delta)
+        if d > 20:
+            position = position.linear_interpolate(path[0], speed*delta/alert_vel)
         else:
             path.remove(0)
     else:
@@ -87,16 +106,16 @@ func _process(delta):
     
     
     if vision_raycast.is_colliding():
-        if vision_raycast.get_collider().name != last_print:
-            last_print = vision_raycast.get_collider().name
-            print(last_print)
+        print(vision_raycast.get_collider().name)
+#        if vision_raycast.get_collider().name != last_print:
+#            last_print = vision_raycast.get_collider().name
+#            print(last_print)
         if vision_raycast.get_collider().name == 'Player':
             if is_visible == false:
                 fade_visibilty(1)    
         elif is_visible == true:
             fade_visibilty(0)
-    elif is_visible == true:
-        fade_visibilty(0)
+    
         
     if path.size() > 1:
         var side = path[1] - path[0]
@@ -113,3 +132,12 @@ func _process(delta):
             Tween.TRANS_LINEAR, Tween.EASE_OUT)
         
         effect.start()
+    update()
+
+
+func _on_path_timer_timeout():
+    update_path()
+
+
+func _on_alert_timer_timeout():
+    alert = false
